97f7a416b416bc65f487e8e7ab5e169feb60aeae
Julio Facal <starlyn.dev@gmail.com>
2022-10-24 14:07:03 -0300

    adicionando criptografia + criar_banco.py

--- views_user.py
+++ views_user.py
@@ -2,6 +2,7 @@ from jogoteca import app
 from flask import render_template, request, redirect, session, flash, url_for
 from helpers import FormularioUsuario
 from models import Usuarios
+from flask_bcrypt import check_password_hash
 
 @app.route('/login')
 def login():
@@ -19,14 +20,14 @@ def logoff():
 def autenticar():
     form = FormularioUsuario(request.form)
     usuario = Usuarios.query.filter_by(nickname=form.nickname.data).first()
-    if usuario:
-            if form.senha.data == usuario.senha:
-                session['usuario_logado'] = usuario.nickname
-                flash(usuario.nickname + ' logado com sucesso!')
-                proxima_pagina = request.form['proxima']
-                if proxima_pagina == 'None':
-                    return redirect(url_for('index'))
-                return redirect(proxima_pagina)
+    senha = check_password_hash(usuario.senha, form.senha.data)
+    if usuario and senha:
+        session['usuario_logado'] = usuario.nickname
+        flash(usuario.nickname + ' logado com sucesso!')
+        proxima_pagina = request.form['proxima']
+        if proxima_pagina == 'None':
+            return redirect(url_for('index'))
+        return redirect(proxima_pagina)
     else:
         flash('Não logado, tente novamente!')
         return redirect(url_for('login'))
