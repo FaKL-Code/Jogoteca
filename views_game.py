adda92a2664eb2895a691acfb2495775b21a1156
Julio Facal <starlyn.dev@gmail.com>
2022-10-24 13:39:42 -0300

    refatorando o codigo

--- views_game.py
+++ views_game.py
@@ -2,8 +2,8 @@ import time
 
 from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
 from jogoteca import app, db
-from models import Jogos, Usuarios
-from helpers import FormularioJogo, deleta_arquivo, recupera_imagem, FormularioUsuario
+from models import Jogos
+from helpers import FormularioJogo, deleta_arquivo, recupera_imagem
 
 
 @app.route('/')
@@ -86,34 +86,6 @@ def excluir(id):
     db.session.commit()
     flash('Jogo excluído com sucesso!')    
     return redirect(url_for('index'))
-
-@app.route('/login')
-def login():
-    proxima = request.args.get('proxima')
-    form = FormularioUsuario()
-    return render_template('login.html', proxima=proxima, form=form)
-
-@app.route('/logoff')
-def logoff():
-    session['usuario_logado'] = None
-    flash('Nenhum usuário logado')
-    return redirect(url_for('index'))
-
-@app.route('/autenticar', methods=['POST',])
-def autenticar():
-    form = FormularioUsuario(request.form)
-    usuario = Usuarios.query.filter_by(nickname=form.nickname.data).first()
-    if usuario:
-            if form.senha.data == usuario.senha:
-                session['usuario_logado'] = usuario.nickname
-                flash(usuario.nickname + ' logado com sucesso!')
-                proxima_pagina = request.form['proxima']
-                if proxima_pagina == 'None':
-                    return redirect(url_for('index'))
-                return redirect(proxima_pagina)
-    else:
-        flash('Não logado, tente novamente!')
-        return redirect(url_for('login'))
     
 @app.route('/uploads/<nome_arquivo>')
 def imagem(nome_arquivo):
